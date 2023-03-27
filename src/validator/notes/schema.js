a8dd3118bcd8a3bd364d3d7afc3d9710bc419bb4
asstola <asyahrulstola@gmail.com>
2023-03-27 22:09:40 +0800

    initial commit

---
+++ src/validator/notes/schema.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable indent */
+const Joi = require('joi');
+
+const NotePayloadSchema = Joi.object({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+});
+
+module.exports = { NotePayloadSchema };
